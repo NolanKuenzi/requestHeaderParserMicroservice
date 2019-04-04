4a5863cd422ac2996c214e311224ebcbc194f6b6
Nolan Kuenzi <mr.nolank@gmail.com>
2019-04-03 23:57:36 -0700

    ip update

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get("/", function(req, res) {
 });
 
 app.get("/api/whoami", function(req, res) {
-  res.json({"ipaddress":req.connection.remoteAddress, "language":req.headers["accept-language"],
+  res.json({"ipaddress":req.headers["X-Forwarded-For"], "language":req.headers["accept-language"],
   "software":req.headers["user-agent"]});
 });
 
