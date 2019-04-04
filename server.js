e5d3e9c311936944747f6d89a315d8ee3d970de2
Nolan Kuenzi <mr.nolank@gmail.com>
2019-04-03 23:41:34 -0700

    initial upload

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+
+var cors = require('cors');
+app.use(cors({optionSuccessStatus: 200}));
+
+app.use(express.static('public'));
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+app.get("/api/whoami", function(req, res) {
+  res.json({"ipaddress":req.connection.remoteAddress, "language":req.headers["accept-language"],
+  "software":req.headers["user-agent"]});
+});
+
+const port = process.env.PORT || 3000;
+
+const listener = app.listen(port, function() {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
